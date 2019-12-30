75ff8b49e81fc7a2d7c19a20fec66543b57b24d4
pranshu_shrivastava <pranshu.shrivastava@intellectdesign.com>
2019-12-30 19:27:36 +0530

    modifications

--- src/main/java/com/inmobi/gitprofiler/Application.java
+++ src/main/java/com/inmobi/gitprofiler/Application.java
@@ -2,6 +2,8 @@ package com.inmobi.gitprofiler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  *
@@ -9,8 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-class Application {
-	
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
